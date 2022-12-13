88ad041bf9ba201696a92df031e9e19576f9922b
EnricoValastro <79710717+EnricoValastro@users.noreply.github.com>
2022-12-13 19:14:53 +0100

    Lab4_init

---
+++ Lab4/materiale-vue01/esercizio1-2/src/routes/moviesRoutes.js
@@ -0,0 +1,19 @@
+const moviesController = require('../controllers/moviesController');
+
+module.exports = (app)=> {
+
+	app.route('/')
+		.get(moviesController.show_index);
+
+	app.route('/movies-crud')
+		.get(moviesController.show_crud);
+		
+	app.route('/api/movies')
+		.get(moviesController.list_movies)
+		.post(moviesController.create_movie);
+
+	app.route('/api/movies/:id')
+		.get(moviesController.read_movie)
+		.put(moviesController.update_movie)
+		.delete(moviesController.delete_movie);
+};
