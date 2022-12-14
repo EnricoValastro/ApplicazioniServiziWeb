18e8767609f170014e25244c76f35cc559b17457
EnricoValastro <79710717+EnricoValastro@users.noreply.github.com>
2022-12-14 18:17:43 +0100

    Lab5_createVueRouter

---
+++ Lab5/MySolutions/Esercizio1/src/controllers/moviesController.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose');
+Movie = require("../models/moviesModel.js")(mongoose);
+
+exports.show_index = (req, res)=> {
+	res.sendFile(appRoot  + '/www/index.html');
+};
+
+exports.list_movies = (req, res)=> {
+	Movie.find({}, (err, movie)=> {
+		if (err)
+			res.send(err);
+		res.json(movie);
+	});
+};
+
+exports.read_movie = (req, res)=> {
+	/*
+	TODO cast req.params.id to ObjectId
+	*/
+	Movie.findById(req.params.id, (err, movie)=> {
+		if (err)
+			res.send(err);
+		else{
+			if(movie==null){
+				res.status(404).send({
+					description: 'Movie not found'
+				});
+			}
+			else{
+				res.json(movie);
+			}
+		}
+	});
+};
+
+exports.create_movie = (req, res)=> {
+	var new_movie = new Movie(req.body);
+	new_movie.save((err, movie)=> {
+		if (err)
+			res.send(err);
+		res.status(201).json(movie);
+	});
+};
+
+exports.update_movie = (req, res)=> {
+	Movie.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}, function(err, movie) {
+		if (err)
+			res.send(err);
+		else{
+			if(movie==null){
+				res.status(404).send({
+					description: 'Movie not found'
+				});
+			}
+			else{
+				res.json(movie);
+			}
+		}
+	});
+};
+
+exports.delete_movie = (req, res)=> {
+	Movie.deleteOne({_id: req.params.id}, (err, result)=> {
+		if (err)
+			res.send(err);
+		else{
+			if(result.deletedCount==0){
+				res.status(404).send({
+					description: 'Movie not found'
+				});
+			}
+			else{
+				res.json({ message: 'Task successfully deleted' });
+			}
+		}
+  });
+};
